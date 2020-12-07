6b1d1900409723e4f64447243fa20dc7f6cb5c40
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2020-12-07 16:57:43 +0100

    added main file

---
+++ main.rb
@@ -0,0 +1,146 @@
+module Enumerable
+  # 1.my_each
+  def my_each
+    for i in self
+      yield(i) if block_given?
+    end
+    self
+  end
+
+  # 2. my_each_with_index
+  def my_each_with_index
+    for i in (0..self).size- 1
+      yield(self[i], i) if block_given?
+    end
+    self
+  end
+
+  # 3. my_select
+  def my_select
+    if block_given?
+      result = []
+      my_each do |value|
+        result << value if yield(value)
+      end
+    end
+    result
+  end
+
+  # 4. my_all
+  def my_all?(parameter = nil)
+    result = true
+    if block_given?
+      my_each { |value| return false unless yield(value) }
+    else
+      case parameter
+      when nil
+        my_each { |value| return false if value.nil? || !value }
+      when Regexp
+        my_each { |value| return false unless value =~ parameter }
+      when Class
+        my_each { |value| return false unless value.is_a? parameter }
+      else
+        my_each { |value| return false unless value == parameter }
+      end
+    end
+    result
+  end
+
+  # 4. my_any
+  def my_any?(parameter = nil)
+    result = false
+    if block_given?
+      my_each do |x|
+        result = true if yield(x)
+      end
+      result
+    else
+      case parameter
+      when nil
+        my_each { |x| return true unless x.nil? || !x }
+      when Regexp
+        my_each { |x| return true if x =~ parameter }
+      when Class
+        my_each { |x| return true if x.is_a? parameter }
+      else
+        my_each { |element| result = true unless element.nil? }
+      end
+    end
+    result
+  end
+
+  # 6.my_none?
+  def my_none?(parameter = nil)
+    result = true
+    if block_given?
+      my_each do |x|
+        result = false if yield(x)
+      end
+      result
+    else
+      case parameter
+      when nil
+        my_each { |x| return false unless x.nil? || !x }
+      when Regexp
+        my_each { |x| return false if x =~ parameter }
+      when Class
+        my_each { |x| return false if x.is_a? parameter }
+      else
+        my_each { |x| return false if x == parameter }
+      end
+    end
+    result
+  end
+
+  # 7.my_count
+  def my_count(parameter = nil)
+    counter = 0
+    if block_given?
+      my_each do |x|
+        if yield(x)
+          counter += 1
+        end
+      end
+      counter
+    else
+      case parameter
+      when nil
+        size
+      when Numeric
+        my_each { |x| counter += 1 if parameter == x }
+        counter
+      end
+    end
+  end
+
+  # 8.my_map
+  def my_map(parameter = nil)
+    return to_enum unless block_given?
+    result_arr = []
+    my_each { |x| result_arr << yield(x) } if parameter.nil?
+    my_each { |x| result_arr << parameter.call(x) } unless parameter.nil?
+    result_arr
+  end
+
+  # 9.my_inject
+  def my_inject(*parameter)
+    (raise LocalJumpError if !block_given? && parameter[0].nil? && parameter[1].nil?)
+    if block_given?
+      net = parameter[0] ? yield(first, parameter[0]) : first
+      drop(1).my_each { |item| net = yield(net, item) }
+    elsif parameter[0]
+      if parameter[1]
+        net = parameter[0]
+        my_each { |item| net = net.send(parameter[1], item) }
+      else
+        net = first
+        drop(1).my_each { |item| net = net.send(parameter[0], item) }
+      end
+    end
+    net
+  end
+end
+
+def multiply_els(parameter)
+  parameter.my_inject { |sum, x| sum * x }
+end
